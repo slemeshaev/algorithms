8e6950a17a5daed9f48d4d4703e8b9f50ad9c4a6
slemeshaev <lemeshaevs@gmail.com>
2019-02-02 02:27:16 +0300

    Решил во второй задаче 2 подзадачи

--- HW_2_Lemeshaev_Stanislav/HW_2_Lemeshaev_Stanislav/main.c
+++ HW_2_Lemeshaev_Stanislav/HW_2_Lemeshaev_Stanislav/main.c
@@ -19,20 +19,53 @@
 
 #include <stdio.h>
 int DecToBin(int n);
+int cycleDegree(int a, int b);
+int recDegree(int a, int b);
 
 int main(int argc, const char * argv[]) {
-    printf("Enter your number: ");
-    int m;
-    scanf("%d", &m);
-    printf("Your binary number is %d\n", DecToBin(m));
+//    printf("Enter your number: ");
+//    int m;
+//    scanf("%d", &m);
+//    printf("Your binary number is %d\n", DecToBin(m));
+    printf("Enter your number and degree: ");
+    int x, y;
+    scanf("%d%d", &x, &y);
+    printf("%d to degree %d is %d\n", x, y, cycleDegree(x, y));
+    printf("%d to degree %d is %d\n", x, y, recDegree(x, y));
     return 0;
 }
 
 // 1. Реализовать функцию перевода из десятичной системы в двоичную, используя рекурсию.
-int DecToBin(int dec)
-{
+int DecToBin(int dec) {
     if (dec < 2)
         return dec;
     else
         return DecToBin(dec / 2) * 10 + dec % 2;
+}
+
+// 2. Реализовать функцию возведения числа a в степень b:
+
+// c. *рекурсивно, используя свойство четности степени.
+
+// a. без рекурсии;
+int cycleDegree(int a, int b) {
+    int res = 1;
+    while (b > 0) {
+        res = res * a;
+        b--;
+    }
+    return res;
+}
+
+// b. рекурсивно;
+int recDegree(int a, int b) {
+    if (b == 0) {
+        return 1;
+    }
+    if (b == 1) {
+        return a;
+    }
+    else {
+        return a * recDegree(a, b - 1);
+    }
 }
