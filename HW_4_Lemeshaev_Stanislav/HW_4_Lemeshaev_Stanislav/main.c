70be5eb6c280b1bb4474b60474287812b78a70a5
slemeshaev <lemeshaevs@gmail.com>
2019-02-11 23:37:36 +0300

    Создал проект для 4 домашнего задания по Алгоритмам и структурам данных

---
+++ HW_4_Lemeshaev_Stanislav/HW_4_Lemeshaev_Stanislav/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HW_4_Lemeshaev_Stanislav
+//
+//  Created by Станислав Лемешаев on 11/02/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
