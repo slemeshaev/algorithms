a3d0176716c31c7eae4a31197f9fb2f363915ecd
slemeshaev <lemeshaevs@gmail.com>
2019-02-24 09:39:29 +0300

    Created files for hw 8

---
+++ HW_8_Lemeshaev_Stanislav/HW_8_Lemeshaev_Stanislav/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HW_8_Lemeshaev_Stanislav
+//
+//  Created by Станислав Лемешаев on 24/02/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
